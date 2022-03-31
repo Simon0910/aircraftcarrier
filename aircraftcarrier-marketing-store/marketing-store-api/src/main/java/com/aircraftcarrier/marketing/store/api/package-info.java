ec76014bb9c38cbc1bd38a6f9f704d11d735221a
liuzhipeng <ext.liuzhipeng5@jd.com>
2022-03-31 20:48:05 +0800

    fix

--- aircraftcarrier-marketing-store/marketing-store-api/src/main/java/com/aircraftcarrier/marketing/store/api/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 对外系统提供的 API
- * 除lombok,swagger文档, 没有任何依赖
- * 入参请求全部使用实体类, 并且不要有继承关系, 包含 requestId, operator
- * 出参封装统一对象, 并且不要有继承关系, 包含 responseId, code, message, data 必选字段
- *
- * @author lzp
- */
-package com.aircraftcarrier.marketing.store.api;
