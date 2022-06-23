667c6d07d83f2c52158fddf645ef6bf169bbd797
lzp <liuzhipeng91@gmail.com>
2022-06-23 16:30:42 +0800

    fix

--- aircraftcarrier-generator/src/main/java/com/aircraftcarrier/generator/Generator.java
+++ aircraftcarrier-generator/src/main/java/com/aircraftcarrier/generator/Generator.java
@@ -10,12 +10,12 @@ package com.aircraftcarrier.generator;
  * @author lzp
  */
 public class Generator {
-    private static final String URL = "jdbc:mysql://localhost:3306/aircraftcarrier?allowMultiQueries=true&useSSL=false&rewriteBatchedStatements=true&zeroDateTimeBehavior=convertToNull&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/aircraftcarrier?allowMultiQueries=true&useSSL=false&rewriteBatchedStatements=true&zeroDateTimeBehavior=convertToNull&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "123456";
+    private static final String PASSWORD = "12345678";
     private static final String AUTHOR = "lzp";
     private static final String[] TABLE_PREFIX = new String[]{"portal_", "farm_"};
-    private static final String OUTPUT_DIR = "D:\\aircraftcarrier\\aircraftcarrier-marketing-store";
+    private static final String OUTPUT_DIR = "/Users/zhipengliu/IdeaProjects/aircraftcarrier/aircraftcarrier-marketing-store";
     private static final String PARENT = "com.aircraftcarrier.marketing.store";
 
 
