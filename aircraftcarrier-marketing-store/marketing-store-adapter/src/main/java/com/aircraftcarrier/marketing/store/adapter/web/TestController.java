e1eb29d71fdded5735fede0a1c9c112a40e3d6f1
liuzhipeng <liuzhipeng91@gmail.com>
2022-08-10 19:01:04 +0800

    fix

--- aircraftcarrier-marketing-store/marketing-store-adapter/src/main/java/com/aircraftcarrier/marketing/store/adapter/web/TestController.java
+++ aircraftcarrier-marketing-store/marketing-store-adapter/src/main/java/com/aircraftcarrier/marketing/store/adapter/web/TestController.java
@@ -15,7 +15,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
