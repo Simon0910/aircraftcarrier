df44ef2f0b9a05672a7aca302c715a55bfc8b47d
lzp <1906846616@qq.com>
2022-10-26 23:20:31 +0800

    fix

--- aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/scheduling/TaskService.java
+++ aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/scheduling/TaskService.java
@@ -147,7 +147,13 @@ public class TaskService {
         log.info("schedule canceled... {}", cancel);
         log.info("schedule is done: {}", scheduledFuture.isDone());
 
-        dynamicTaskMap.remove(taskName);
+        // 如果正在等待，需要手动移除
+        AbstractTask abstractTask = dynamicTaskMap.get(taskName);
+        if (abstractTask.isWaiting()) {
+            dynamicTaskMap.remove(taskName);
+        }
+        // 如果正在运行，任务自己移除
+        // dynamicTaskMap.remove(taskName);
         scheduledMap.remove(taskName);
         // scheduledFuture 中的 task.state = RUNNING 为什么不是INTERRUPTED？ cancel是在RUNNING时候异步设置
         log.info("remove schedule task: {}", scheduledFuture);
@@ -242,7 +248,8 @@ public class TaskService {
     private void removeManualScheduler(Map<String, FutureTask<?>> manualFutureTaskMap, String taskName, Future<?> f, Consumer<Void> message) {
         synchronized (taskName.intern()) {
             if (f == manualFutureTaskMap.get(taskName)) {
-                manualDynamicTaskMap.remove(taskName);
+                // 任务自己移除
+                // manualDynamicTaskMap.remove(taskName);
                 manualFutureTaskMap.remove(taskName);
                 message.accept(null);
             }
