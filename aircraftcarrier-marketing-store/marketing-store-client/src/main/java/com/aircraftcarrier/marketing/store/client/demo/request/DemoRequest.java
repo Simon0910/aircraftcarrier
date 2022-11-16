1d063de70fada28842b04bc844335dec8b75cd97
lzp <1906846616@qq.com>
2022-11-16 16:04:35 +0800

    注释

--- aircraftcarrier-marketing-store/marketing-store-client/src/main/java/com/aircraftcarrier/marketing/store/client/demo/request/DemoRequest.java
+++ aircraftcarrier-marketing-store/marketing-store-client/src/main/java/com/aircraftcarrier/marketing/store/client/demo/request/DemoRequest.java
@@ -25,17 +25,36 @@ public class DemoRequest {
     @InEnum(DemoEnum.class)
     private Integer demoEnum;
 
+    /**
+     * 可接收：
+     * "2022-11-16 15:55:02"
+     * "2022-11-16"
+     * 返回：
+     * 2022-11
+     */
     @JSONField(format = DateTimeUtil.STANDARD_FORMAT)
     @JsonFormat(pattern = "yyyy-MM", timezone = "GMT+8")
     private Date time;
 
+    /**
+     * 可接收：
+     * {@link Timestamp2DateDeserializer} 时间戳 1668585302000
+     * 返回：
+     * {@link Date2ShortStringSerializer} yyyy-MM-dd
+     */
     @JsonSerialize(using = Date2ShortStringSerializer.class)
     @JsonDeserialize(using = Timestamp2DateDeserializer.class)
     private Date time2;
 
     /**
-     * 全局默认
-     * {@link SerializerConfiguration#jsonCustomizer() }
+     * 全局默认：
+     * 接受：
+     * "2022-11-16 15:55:02"
+     * 返回：
+     * "2022-11-16 15:55:02"
+     * <p>
+     * 此配置方式会改变源码原有逻辑 {@link SerializerConfiguration#jsonCustomizer() } 相当于重写顶层默认逻辑，导致@JsonFormat失效
+     * 以下方式只替换部分实现，不改变程序执行逻辑，不影响@JsonFormat，方法为spring提供的扩展入口
      * {@link com.aircraftcarrier.marketing.store.adapter.config.WebMvcConfiguration#extendMessageConverters(java.util.List) }
      */
     private Date time3;
