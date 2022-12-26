f8e4c82b63a9b9039ee623e231aa1974c2aa9bdc
lzp <1906846616@qq.com>
2022-12-26 16:59:53 +0800

    fix

--- aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/tookit/MapUtil.java
+++ aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/tookit/MapUtil.java
@@ -9,6 +9,9 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author lzp
  */
 public class MapUtil {
+
+    static final double DEFAULT_LOAD_FACTOR = 0.75d;
+
     /**
      * MapUtil
      */
@@ -20,18 +23,25 @@ public class MapUtil {
     }
 
     public static <K, V> Map<K, V> newHashMap(int expectedSize) {
-        return new HashMap<>(capacity(expectedSize));
+        return new HashMap<>(calculateHashMapCapacity(expectedSize));
     }
 
     public static <K, V> Map<K, V> newConcurrentHashMap(int expectedSize) {
-        return new ConcurrentHashMap<>(capacity(expectedSize));
+        return new ConcurrentHashMap<>(calculateHashMapCapacity(expectedSize));
     }
 
     public static <K, V> Map<K, V> newLinkedHashMap(int expectedSize) {
-        return new LinkedHashMap<>(capacity(expectedSize));
+        return new LinkedHashMap<>(calculateHashMapCapacity(expectedSize));
     }
 
-    public static int capacity(int expectedSize) {
-        return expectedSize << 1;
+    /**
+     * Calculate initial capacity for HashMap based classes, from expected size and default load factor (0.75).
+     *
+     * @param numMappings the expected number of mappings
+     * @return initial capacity for HashMap based classes.
+     * @since 19
+     */
+    public static int calculateHashMapCapacity(int numMappings) {
+        return (int) Math.ceil(numMappings / DEFAULT_LOAD_FACTOR);
     }
 }
