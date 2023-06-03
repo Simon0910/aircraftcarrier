05289f1ea6349f9ee4591cc9c27509f5c656a83b
liuzhipeng <liuzhipeng91@gmail.com>
2023-06-03 14:07:14 +0800

    LockUtil2 release

--- aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/cache/LockUtil2.java
+++ aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/cache/LockUtil2.java
@@ -1,6 +1,7 @@
 package com.aircraftcarrier.framework.cache;
 
 import com.aircraftcarrier.framework.cache.suport.MyLockTemplate;
+import com.aircraftcarrier.framework.exception.LockNotAcquiredException;
 import com.aircraftcarrier.framework.tookit.ApplicationContextUtil;
 import com.baomidou.lock.LockInfo;
 import com.baomidou.lock.LockTemplate;
@@ -21,9 +22,14 @@ public class LockUtil2 {
     private static final ThreadLocal<LockInfo> THREAD_LOCAL = new ThreadLocal<>();
     private static final Map<String, ReentrantLock> LOCAL_LOCK_CACHE = Maps.newConcurrentMap();
 
+    private LockUtil2() {
+    }
+
     private static String getKey(String lockKey) {
         return getEnv() + lockKey;
     }
+
+
     public static boolean tryLock(String lockKey) {
         lockKey = getKey(lockKey);
         long expire = 30000; // 默认30秒自动失效
@@ -40,7 +46,7 @@ public class LockUtil2 {
             return false;
         } catch (Exception e) {
             log.error("tryLock error key [{}] ", lockKey, e);
-            throw new RuntimeException(e);
+            throw new LockNotAcquiredException(e);
         }
     }
 
@@ -54,46 +60,54 @@ public class LockUtil2 {
      */
     public static boolean tryLock(String lockKey, long timeout, TimeUnit unit) {
         lockKey = getKey(lockKey);
-        long expire = 30000; // 默认30秒自动失效
-        long acquireTimeout = unit.toMillis(timeout);
-        long retryInterval = 20;
-
         ReentrantLock writeKeyLock = getWriteLock(lockKey);
-        long start = System.currentTimeMillis();
         try {
-            while (true) {
-                boolean ok = writeKeyLock.tryLock(timeout, unit);
-                if (ok) {
-                    // continue get redis lock
-                    break;
-                }
-                if (System.currentTimeMillis() - start >= acquireTimeout) {
-                    log.info("timeout...");
-                    return false;
-                }
-                log.info("retry...");
-                TimeUnit.MILLISECONDS.sleep(retryInterval);
+            if (!writeKeyLock.tryLock(timeout, unit)) {
+                log.info("timeout...");
+                return false;
             }
-        } catch (Exception e) {
-            log.error("tryLockTimeout error key [{}] ", lockKey, e);
+            // continue get redis lock...
+        } catch (InterruptedException e) {
+            log.error("tryLockTimeout error interrupted key [{}] ", lockKey, e);
+            Thread.currentThread().interrupt();
             return false;
         }
 
+        long expire = 30000; // 默认30秒自动失效
+        long acquireTimeout = unit.toMillis(timeout);
+        // 假设百分之99的场景的并发key, 过几毫秒就可以成功获取！ 超时之前最多获取 n次: retryInterval = acquireTimeout / n;
+        // 假设 timout = 12s, n = 3, 4s重试一次, 共重试3次
+        // |--- --- --- ---|--- --- --- ---|--- --- --- ---|
+        // 快速重试2次, 实际重试2次, 共重试4次
+        // |---|---|--- --- --- ---|--- --- --- ---|--- ---
+        long retryInterval = acquireTimeout / 3;
+        int retryCount = 0;
+        int fastRetryCount = 2;
         try {
-            if (System.currentTimeMillis() - start < acquireTimeout) {
+            long start = System.currentTimeMillis();
+            do {
                 LockInfo lockInfo = getMyLockTemplate().lockPlus(lockKey, expire, acquireTimeout, retryInterval, null);
                 if (lockInfo != null) {
                     THREAD_LOCAL.set(lockInfo);
                     return true;
                 }
-                TimeUnit.MILLISECONDS.sleep(retryInterval);
-            }
+                if (retryCount < fastRetryCount) {
+                    retryCount++;
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } else {
+                    TimeUnit.MILLISECONDS.sleep(retryInterval);
+                }
+            } while (System.currentTimeMillis() - start < acquireTimeout);
             log.info("tryLock timeout...");
             writeKeyLock.unlock();
             if (!writeKeyLock.hasQueuedThreads() && !writeKeyLock.isLocked()) {
                 log.info("tryLock timeout removeWriteLock [{}] ", lockKey);
                 removeWriteLock(lockKey);
             }
+            return false;
+        } catch (InterruptedException e) {
+            log.error("tryLock error interrupted key [{}] ", lockKey, e);
+            Thread.currentThread().interrupt();
             return false;
         } catch (Exception e) {
             log.error("tryLock error key [{}] ", lockKey, e);
@@ -102,7 +116,7 @@ public class LockUtil2 {
                 log.info("tryLock error removeWriteLock [{}] ", lockKey);
                 removeWriteLock(lockKey);
             }
-            throw new RuntimeException(e);
+            throw new LockNotAcquiredException(e);
         }
     }
 
@@ -133,16 +147,15 @@ public class LockUtil2 {
             log.error("unLock error key [{}] ", lockKey, e);
         } finally {
             ReentrantLock writeKeyLock = LOCAL_LOCK_CACHE.get(lockKey);
-            if (writeKeyLock == null) {
-                return;
-            }
-            if (writeKeyLock.isHeldByCurrentThread()) {
-                log.info("unLock writeKeyLock [{}] ", lockKey);
-                writeKeyLock.unlock();
-            }
-            if (!writeKeyLock.hasQueuedThreads()) {
-                log.info("unLock removeWriteLock [{}] ", lockKey);
-                removeWriteLock(lockKey);
+            if (writeKeyLock != null) {
+                if (writeKeyLock.isHeldByCurrentThread()) {
+                    log.info("unLock writeKeyLock [{}] ", lockKey);
+                    writeKeyLock.unlock();
+                }
+                if (!writeKeyLock.hasQueuedThreads()) {
+                    log.info("unLock removeWriteLock [{}] ", lockKey);
+                    removeWriteLock(lockKey);
+                }
             }
         }
     }
@@ -156,7 +169,7 @@ public class LockUtil2 {
     }
 
     private static class ResourceHolder {
-        private static final String ENV = ApplicationContextUtil.getApplicationContext().getEnvironment().getActiveProfiles()[0] + ":";
         public static final MyLockTemplate myLockTemplate = (MyLockTemplate) ApplicationContextUtil.getBean(LockTemplate.class); // This will be lazily initialised
+        private static final String ENV = ApplicationContextUtil.getApplicationContext().getEnvironment().getActiveProfiles()[0] + ":";
     }
 }
