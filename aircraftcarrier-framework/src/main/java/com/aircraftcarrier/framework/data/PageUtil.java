02779ad60850f6d807d97a10c77ccdc9638b3640
liuzhipeng <liuzhipeng91@gmail.com>
2025-04-13 13:34:59 +0800

    fix

--- aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/data/PageUtil.java
+++ aircraftcarrier-framework/src/main/java/com/aircraftcarrier/framework/data/PageUtil.java
@@ -3,7 +3,7 @@ package com.aircraftcarrier.framework.data;
 import com.aircraftcarrier.framework.model.request.PageQuery;
 import com.aircraftcarrier.framework.model.Page;
 import com.github.pagehelper.ISelect;
-import com.github.pagehelper.page.PageMethod;
+import com.github.pagehelper.PageHelper;
 
 import java.io.Serializable;
 
@@ -16,12 +16,22 @@ public class PageUtil {
     }
 
     public static <R extends Serializable, E> Page<R> getPage(PageQuery pageQuery, ISelect select, Class<R> modelClass) {
-        com.github.pagehelper.Page<E> page = PageMethod.startPage(pageQuery.getPageNum(), pageQuery.getPageSize(), true).doSelectPage(select);
+        com.github.pagehelper.Page<E> page = PageHelper.startPage(pageQuery.getPageNum(), pageQuery.getPageSize(), true);
+        safeExecute(select);
         return Page.build(page.getResult(), page.getTotal(), modelClass);
     }
 
     public static <E extends Serializable> Page<E> getPage(PageQuery pageQuery, ISelect select) {
-        com.github.pagehelper.Page<E> page = PageMethod.startPage(pageQuery.getPageNum(), pageQuery.getPageSize(), true).doSelectPage(select);
+        com.github.pagehelper.Page<E> page = PageHelper.startPage(pageQuery.getPageNum(), pageQuery.getPageSize(), true);
+        safeExecute(select);
         return Page.build(page.getResult(), page.getTotal());
+    }
+
+    private static void safeExecute(ISelect select) {
+        try {
+            select.doSelect();
+        } finally {
+            PageHelper.clearPage();
+        }
     }
 }
